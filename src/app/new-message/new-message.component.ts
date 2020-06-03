32dd85086d23c9fb2e23d314437a696c1e64b6aa
Aljona Trofimova <aljonatrofimova@gmail.com>
2020-06-03 21:47:41 +0300

    Code formatting

--- src/app/new-message/new-message.component.ts
+++ src/app/new-message/new-message.component.ts
@@ -1,58 +1,58 @@
-import {Component} from '@angular/core';
-import {NewMessageEvent} from '../shared/model/new-message-event.model';
-import {NewMessageService} from '../shared/service/new-message.service';
-import {NewMessageEventService} from '../shared/service/new-message-event.service';
-import {HttpErrorResponse} from '@angular/common/http';
+import { Component } from '@angular/core';
+import { NewMessageEvent } from '../shared/model/new-message-event.model';
+import { NewMessageService } from '../shared/service/new-message.service';
+import { NewMessageEventService } from '../shared/service/new-message-event.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
-  selector: 'app-new-message',
-  templateUrl: './new-message.component.html',
-  styleUrls: ['./new-message.component.scss'],
+	selector: 'app-new-message',
+	templateUrl: './new-message.component.html',
+	styleUrls: ['./new-message.component.scss'],
 })
 export class NewMessageComponent {
-  newMessageCreated: boolean;
-  validationMessage: string = null;
-  response: HttpErrorResponse = null;
+	newMessageCreated: boolean;
+	validationMessage: string = null;
+	response: HttpErrorResponse = null;
 
-  constructor(
-    private newMessageService: NewMessageService,
-    private newMessageEventService: NewMessageEventService,
-  ) {
-  }
+	constructor(
+		private newMessageService: NewMessageService,
+		private newMessageEventService: NewMessageEventService,
+	) {}
 
-  createMessage(messageText: string) {
-    this.response = null;
-    if (messageText === null) {
-      this.validationMessage = 'Cannot send empty message';
-      return;
-    } else if (messageText.length < 1) {
-      this.validationMessage = 'Cannot send empty message';
-      return;
-    }
+	createMessage(messageText: string) {
+		this.response = null;
+		if (messageText === null) {
+			this.validationMessage = 'Cannot send empty message';
+			return;
+		} else if (messageText.length < 1) {
+			this.validationMessage = 'Cannot send empty message';
+			return;
+		}
 
-    this.newMessageCreated = false;
+		this.newMessageCreated = false;
 
-    this.newMessageService.create(messageText).subscribe(
-      () => {
-        this.validationMessage = 'Message was created';
-      },
-      error => {
-        this.validationMessage = this.generateValidationMessage(error);
-      },
-    );
-    this.newMessageEventService.send(new NewMessageEvent(true));
-  }
+		this.newMessageService.create(messageText).subscribe(
+			() => {
+				this.validationMessage = 'Message was created';
+			},
+			error => {
+				this.validationMessage = this.generateValidationMessage(error);
+			},
+		);
+		this.newMessageEventService.send(new NewMessageEvent(true));
+	}
 
-  resetValidationMessage() {
-    this.validationMessage = null;
-  }
+	resetValidationMessage() {
+		this.validationMessage = null;
+	}
 
-  generateValidationMessage(httpErrorResponse: HttpErrorResponse) {
-    if (httpErrorResponse != null)
-      return (
-        'Unexpected error occured with status ' +
-        httpErrorResponse.status +
-        ' and message ' + httpErrorResponse.message
-      );
-  }
+	generateValidationMessage(httpErrorResponse: HttpErrorResponse) {
+		if (httpErrorResponse != null)
+			return (
+				'Unexpected error occured with status ' +
+				httpErrorResponse.status +
+				' and message ' +
+				httpErrorResponse.message
+			);
+	}
 }
